c6451781ef8be1cde24d9a39262de7290c470769
dominnya <dominnya@gmail.com>
2026-03-26 00:01:30 +0400

    chore(Home): ignore rules of hooks for i18n

--- src/app/[lang]/page.tsx
+++ src/app/[lang]/page.tsx
@@ -11,6 +11,7 @@ export default async function Home({
   }>;
 }>) {
   const { lang } = await params;
+  // eslint-disable-next-line react-hooks/rules-of-hooks
   const { t } = await useTranslation(lang, "home");
 
   return (
